667ce9d6c86386f3d3560e05d22153763e9a288f
Frank Fang <FrankFang1990@gmail.com>
2018-01-03 12:18:57 +0800

    第三次实现

--- main.js
+++ main.js
@@ -1,79 +1,73 @@
-let n
-初始化()
-let timer = setInterval(()=>{
-  makeLeave(getImage(n))
-    .one('transitionend', (e)=>{
-      makeEnter($(e.currentTarget))
-    })
-  makeCurrent(getImage(n+1))
-  n += 1
-},2000)
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
 
-document.addEventListener('visibilitychange', function(e){
-  if(document.hidden){
-    window.clearInterval(timer)
-  }else{
-    timer = setInterval(()=>{
-      makeLeave(getImage(n))
-        .one('transitionend', (e)=>{
-          makeEnter($(e.currentTarget))
-        })
-      makeCurrent(getImage(n+1))
-      n += 1
-    },2000)
-  }
+makeFakeSlides()
+$slides.css({transform:'translateX(-400px)'})
+bindEvents()
+$(next).on('click', function(){
+  goToSlide(current+1)
+})
+$(previous).on('click', function(){
+  goToSlide(current-1)
 })
 
+let timer = setInterval(function(){
+  goToSlide(current+1)
+},2000)
+$('.container').on('mouseenter', function(){
+  window.clearInterval(timer)
+}).on('mouseleave', function(){
+  timer = setInterval(function(){
+    goToSlide(current+1)
+  },2000)
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 下面可以不看
-
-function getImage(n){
-  return $(`.images > img:nth-child(${x(n)})`)
+function bindEvents(){
+  $('#buttonWrapper').on('click', 'button', function(e){
+    let $button = $(e.currentTarget) 
+    let index = $button.index()
+    goToSlide(index)
+  })
 }
 
-function x(n){
-  if(n>3){
-    n = n%3
-    if (n===0){
-      n =3
-    }
-  } // n = 1 2 3
-  return n
+//重要
+function goToSlide(index){
+  if(index > $buttons.length-1){
+    index = 0
+  }else if(index <0){
+    index = $buttons.length - 1
+  }
+  console.log('current', 'index')
+  console.log(current, index)
+  if(current === $buttons.length -1 && index === 0){
+    // 最后一张到第一张
+    console.log('here')
+    $slides.css({transform:`translateX(${-($buttons.length + 1) * 400}px)`})
+      .one('transitionend', function(){
+        $slides.hide().offset()
+        $slides.css({transform:`translateX(${-(index+1)*400}px)`}).show()
+      })
+
+  }else if(current === 0 && index === $buttons.length - 1){
+    // 第一张到最后一张
+    $slides.css({transform:`translateX(0px)`})
+      .one('transitionend', function(){
+        $slides.hide().offset()
+        $slides.css({transform:`translateX(${-(index+1)*400}px)`}).show()
+      })
+
+  }else{
+    $slides.css({transform:`translateX(${- (index+1) * 400}px)`})
+  }
+  current = index
 }
 
-function 初始化(){
-  n = 1
-  $(`.images > img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
-}
+function makeFakeSlides(){
+  let $firstCopy = $images.eq(0).clone(true)
+  let $lastCopy = $images.eq($images.length-1).clone(true)
 
-function makeCurrent($node){
-  return $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node){
-  return $node.removeClass('enter current').addClass('leave')
-}
-function makeEnter($node){
-  return $node.removeClass('leave current').addClass('enter')
+  $slides.append($firstCopy)
+  $slides.prepend($lastCopy)
 }
